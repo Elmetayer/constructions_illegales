22dab63a2134e92aa13983118a766c03c1ff5dd5
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-17 00:05:00 +0100

    Update 1_Map_Demo.py

--- pages/1_Map_Demo.py
+++ pages/1_Map_Demo.py
@@ -13,6 +13,10 @@ st.set_page_config(page_title="Map Demo", page_icon="📈")
 st.markdown("# Map Demo")
 st.sidebar.header("Map Demo")
 
+if 'last_clicked' not in st.session_state:
+    st.session_state['last_clicked'] = None
+st.write(st.session_state['last_clicked'])
+
 # recherche de l'adresse dans la barre latérale
 adresse = st.sidebar.text_input('Adresse', 'Champ de Mars, 5 Av. Anatole France, 75007 Paris')
 request_wxs = 'https://wxs.ign.fr/essentiels/geoportail/geocodage/rest/0.1/search?q={}&index=address&limit=1&returntruegeometry=false'.format(
@@ -26,15 +30,16 @@ coords_Lambert = gpd.GeoDataFrame(
      'geometry': [shapely.geometry.Point(X0, Y0)]},
     crs = 'EPSG:2154')
 coords_WSG = coords_Lambert.to_crs('EPSG:4326')
-st.write('Les coordonnées sont: ({}, {})'.format(coords_WSG.geometry[0].x, coords_WSG.geometry[0].y))
 
-# center on Liberty Bell, add marker
+# affichage de la carte et centrage sur l'adresse entrée
 m = folium.Map(location=[coords_WSG.geometry[0].y, coords_WSG.geometry[0].x], zoom_start=16)
 folium.Marker(
     [coords_WSG.geometry[0].y, coords_WSG.geometry[0].x], 
     popup = adresse, 
-    tooltip = adresse).add_to(m)
+    tooltip = '({}, {})'.format(coords_WSG.geometry[0].x, coords_WSG.geometry[0].y)).add_to(m)
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width=725)
-st.write(json.load(st_data))
+st.session_state['last_clicked'] = st_data['last_clicked']
+
+
