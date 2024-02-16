a827e0a36a595d215d269c54885203950133e7a4
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-16 22:50:34 +0100

    Create 1_Map_Demo.py

---
+++ pages/1_Map_Demo.py
@@ -0,0 +1,17 @@
+import folium
+import streamlit as st
+
+from streamlit_folium import st_folium
+
+st.set_page_config(page_title="Cropping Demo", page_icon="📈")
+st.markdown("# Cropping Demo")
+st.sidebar.header("Cropping Demo")
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
