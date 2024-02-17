45db9f0d23230f10620207a3c7d2ddc43f7d92a5
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-17 02:02:02 +0100

    Update 1_Map_Demo.py

--- pages/1_Map_Demo.py
+++ pages/1_Map_Demo.py
@@ -17,10 +17,6 @@ st.sidebar.header("Map Demo")
 CENTER_START = [48.858370, 2.294481]
 if 'last_coords' not in st.session_state:
     st.session_state['last_coords'] = [48.858370, 2.294481]
-if 'search' not in st.session_state:
-    st.session_state['search'] = True
-
-st.write('adresse courante: {}'.format(st.session_state['search']))
 
 def search_adresse():
     request_wxs = 'https://wxs.ign.fr/essentiels/geoportail/geocodage/rest/0.1/search?q={}&index=address&limit=1&returntruegeometry=false'.format(
@@ -35,7 +31,6 @@ def search_adresse():
         crs = 'EPSG:2154')
     coords_WSG = coords_Lambert.to_crs('EPSG:4326')
     st.session_state['last_coords'] = [coords_WSG.geometry[0].y, coords_WSG.geometry[0].x]
-    st.session_state['search'] = True
 
 # recherche de l'adresse dans la barre latérale
 with st.sidebar.form('adresse_search'):
@@ -52,5 +47,5 @@ m = folium.Map(location = CENTER_START, zoom_start = 16)
 out_m = st_folium(m, feature_group_to_add = fg, center = st.session_state['last_coords'], width=725)
 if out_m['last_clicked'] and st.session_state['last_coords'] != [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']] and not st.session_state['search']:
     st.session_state['last_coords'] = [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]
-    st.session_state['last_clicked'] = [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]
+    st.session_state['adresse_text'] = ''
     st.rerun()
