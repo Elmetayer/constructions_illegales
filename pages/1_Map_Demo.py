9ac0735200ce24d596babc9f2353e04de8eaf0f1
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-17 01:17:33 +0100

    Update 1_Map_Demo.py

--- pages/1_Map_Demo.py
+++ pages/1_Map_Demo.py
@@ -15,12 +15,14 @@ st.sidebar.header("Map Demo")
 
 # variables de session
 CENTER_START = [48.858370, 2.294481]
-if 'last_action' not in st.session_state:
-    st.session_state['last_action'] = 'click'
-if 'adresse_coords' not in st.session_state:
-    st.session_state['adresse_coords'] = None
-if 'click_coords' not in st.session_state:
-    st.session_state['click_coords'] = None
+if 'last_coords' not in st.session_state:
+    st.session_state['last_coords'] = 'click'
+if 'first_launch' not in st.session_state:
+    st.session_state['first_launch'] = True
+
+st.write('adresse courante: {}'.format(st.session_state['adresse_coords']))
+st.write('coordonnées click courantes: {}'.format(st.session_state['click_coords']))
+st.write('init: {}'.format(st.session_state['first_launch']))
 
 # recherche de l'adresse dans la barre latérale
 adresse = st.sidebar.text_input('Adresse', None)
@@ -36,24 +38,17 @@ if adresse:
          'geometry': [shapely.geometry.Point(X0, Y0)]},
         crs = 'EPSG:2154')
     coords_WSG = coords_Lambert.to_crs('EPSG:4326')
-    st.session_state['adresse_coords'] = [coords_WSG.geometry[0].y, coords_WSG.geometry[0].x]
-    st.session_state['last_action'] = 'adresse'
+    st.session_state['last_coords'] = [coords_WSG.geometry[0].y, coords_WSG.geometry[0].x]
 
 # affichage de la carte et centrage sur l'adresse entrée
-center = None
-if st.session_state['last_action'] == 'adresse':
-    center = st.session_state['adresse_coords']
-if st.session_state['last_action'] == 'click':
-    center = st.session_state['click_coords']
 fg = folium.FeatureGroup(name = 'centre carte')
 fg.add_child(folium.Marker(
     center, 
     popup = adresse, 
     tooltip = ''))
 m = folium.Map(location = CENTER_START, zoom_start = 16)
-out_m = st_folium(m, feature_group_to_add = fg, center = center, width=725)
-if out_m['last_clicked']:
-    if st.session_state['click_coords'] != [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]:
-        st.session_state['click_coords'] = [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]
-        st.session_state['last_action'] = 'click'
+out_m = st_folium(m, feature_group_to_add = fg, center = st.session_state['last_coords'], width=725)
+if out_m['last_clicked'] and not st.session_state['first_launch']:
+    if st.session_state['last_coords'] != [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]:
+        st.session_state['last_coords'] = [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]
         st.rerun()
