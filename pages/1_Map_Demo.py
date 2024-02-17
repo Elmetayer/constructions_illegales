d17b49aba7fd33a6d6c12939c121847d413f1cd0
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-17 02:02:33 +0100

    Update 1_Map_Demo.py

--- pages/1_Map_Demo.py
+++ pages/1_Map_Demo.py
@@ -45,7 +45,7 @@ fg.add_child(folium.Marker(
     tooltip = ''))
 m = folium.Map(location = CENTER_START, zoom_start = 16)
 out_m = st_folium(m, feature_group_to_add = fg, center = st.session_state['last_coords'], width=725)
-if out_m['last_clicked'] and st.session_state['last_coords'] != [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']] and not st.session_state['search']:
+if out_m['last_clicked'] and st.session_state['last_coords'] != [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]:
     st.session_state['last_coords'] = [out_m['last_clicked']['lat'], out_m['last_clicked']['lng']]
     st.session_state['adresse_text'] = ''
     st.rerun()
