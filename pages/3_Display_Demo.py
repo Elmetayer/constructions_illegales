9396c087ccdac8b4dedc1fac592252213f37d389
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-19 00:14:34 +0100

    Update 3_Display_Demo.py

--- pages/3_Display_Demo.py
+++ pages/3_Display_Demo.py
@@ -17,12 +17,8 @@ st.markdown("# Display Demo")
 st.sidebar.header("Display Demo")
 
 # variables de session
-PIXEL_SCALE = 0.2
-PIXEL_SIZE_DEFAUT = 1000
-
-# taille en pixel
-pixel_size = st.sidebar.slider('Taille (pixel)', 0, 1000, PIXEL_SIZE_DEFAUT, 100)
-st.sidebar.caption('Taille (m): {}'.format(PIXEL_SCALE*pixel_size))
+PIXEL_SIZE_MAX = 1000
+PIXEL_SCALE_REF = 0.2
 
 # calcul de l'image
 coords_bbox_WSG = gpd.GeoDataFrame(
@@ -32,13 +28,20 @@ coords_bbox_WSG = gpd.GeoDataFrame(
       shapely.geometry.Point(st.session_state['bbox'][2], st.session_state['bbox'][3])]},
    crs = 'EPSG:4326')
 ccoords_bbox_Lambert = coords_bbox_WSG.to_crs('EPSG:2154')
-X0 = ccoords_bbox_Lambert.geometry[0].x
-Y0 = ccoords_bbox_Lambert.geometry[1].y
+xmin = ccoords_bbox_Lambert.geometry[0].x
+xmax = ccoords_bbox_Lambert.geometry[1].x
+ymin = ccoords_bbox_Lambert.geometry[0].y
+ymax = ccoords_bbox_Lambert.geometry[1].y
 
-raster_transform = rasterio.transform.Affine(PIXEL_SCALE, 0.0, X0,
-                          0.0, -PIXEL_SCALE, Y0 + PIXEL_SCALE*pixel_size)
-xmin, ymax = raster_transform*(0, 0)
-xmax, ymin = raster_transform*(pixel_size, pixel_size)
+# taille en pixel
+pixel_size_defaut = min(PIXEL_SIZE_MAX, (xmin - xmax)//PIXEL_SCALE_REF)
+pixel_size = st.sidebar.slider('Taille (pixel)', 0, PIXEL_SIZE_MAX, pixel_size_defaut, 100)
+scale = round(pixel_size/(xmin - xmax), 1)
+st.sidebar.caption('Echelle: {} pixel/m'.format(scale))
+if scale != PIXEL_SCALE_REF:
+    st.sidebar.warning('attendion, l\'échelle de référence est {}'.format(PIXEL_SCALE_REF))
+
+# récupération et affichage de l'orthophoto
 request_wms = 'https://data.geopf.fr/wms-r?LAYERS=ORTHOIMAGERY.ORTHOPHOTOS&FORMAT=image/tiff&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&STYLES=&CRS=EPSG:2154&BBOX={},{},{},{}&WIDTH={}&HEIGHT={}'.format(
    xmin, ymin, xmax, ymax, pixel_size, pixel_size)
 response_wms = requests.get(request_wms).content
