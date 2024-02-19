457eb58a9fa6dfd00c3e3f96a0124106e94a9e85
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2024-02-19 22:38:21 +0100

    Mise à jour

--- pages/1_Map_Demo.py
+++ pages/1_Map_Demo.py
@@ -8,6 +8,10 @@ import shapely
 import geopandas as gpd
 
 def search_adresse():
+    '''
+    fonction qui renvoie les coordonnées à partir d'une saisie d'adresse en texte libre
+    utilise l'API de géocodage de l'IGN
+    '''
     if st.session_state['adresse_field']:
         request_wxs = 'https://wxs.ign.fr/essentiels/geoportail/geocodage/rest/0.1/search?q={}&index=address&limit=1&returntruegeometry=false'.format(
             st.session_state['adresse_field'])
@@ -29,6 +33,10 @@ def search_adresse():
             st.session_state['warning_adresse'] = 'aucune adresse trouvée'
 
 def search_lat_lon(lat_lon):
+    '''
+    fonction qui renvoie une adresse à partir de coordonnées
+    utilise l'API de géocodage inversée de l'IGN
+    '''
     result = ADRESSE_DEFAUT
     request_wxs = 'https://wxs.ign.fr/essentiels/geoportail/geocodage/rest/0.1/reverse?lat={}&lon={}&index=address&limit=1&returntruegeometry=false'.format(
         lat_lon[0], lat_lon[1])
@@ -39,6 +47,9 @@ def search_lat_lon(lat_lon):
     return result
 
 def update_point():
+    '''
+    fonction qui met à jour le point valide
+    '''
     if st.session_state['new_point']:
         st.session_state['last_coords'] = st.session_state['new_point']
         st.session_state['adresse_text'] = st.session_state['new_adresse']
@@ -47,6 +58,10 @@ def update_point():
         st.session_state['bbox'] = get_bbox(st.session_state['last_coords'], bbox_size, bbox_mode)
     
 def get_bbox(coords_center, size, mode):
+    '''
+    fonction qui calcule les coordonnées xmin, ymin, xmax, ymax de la bounding box
+    à partir du point de référence, de la taille et du mode
+    '''
     ccoords_center_WSG = gpd.GeoDataFrame(
         {'Nom': ['centre'],
         'geometry': [shapely.geometry.Point(coords_center[1], coords_center[0])]},
