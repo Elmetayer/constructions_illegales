abcda0fdddb1bdd466859f23bca810b75eae56d7
Elmetayer <bruno.lemetayer@tutanota.com>
2024-02-24 22:47:41 +0100

    Update 2_Display_Demo.py

--- pages/2_Display_Demo.py
+++ pages/2_Display_Demo.py
@@ -54,6 +54,9 @@ if 'bbox' not in st.session_state:
 else:
    st.session_state['refresh_bbox'] = (st.session_state['bbox_selected'] != st.session_state['bbox'])*1
 
+st.write(st.session_state['bbox'])
+st.write(st.session_state['bbox_selected'])
+
 # bouton de mise à jour
 load_button = None
 if st.session_state['refresh_bbox'] == 1:
