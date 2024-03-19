c57d7f5a7d12c213af94d26766beaff1eb5ce380
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2024-03-19 14:03:32 +0100

    Update functions.py

--- pages/functions/functions.py
+++ pages/functions/functions.py
@@ -239,7 +239,7 @@ def predict_YOLOv8(image, model, size_model = 512, seuil = 0.01):
   res = model.predict(image, save = False, classes = [0], imgsz = size_model, conf = seuil, verbose=False)
   if len(res[0]) > 0:
     mask = np.sum(res[0].masks.data.cpu().numpy(), 0)
-    mask = np.clip_by_value(mask, 0, 1)
+    mask = np.clip(mask, 0, 1)
   else:
     mask = [np.zeros((size_model, size_model))]
   return [mask]
