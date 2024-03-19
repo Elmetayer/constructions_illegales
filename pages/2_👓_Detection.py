0f16b79ae48be2b5e9c2f083effc1344e3d45ee7
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2024-03-19 15:45:55 +0100

    Update 2_👓_Detection.py

--- pages/2_👓_Detection.py
+++ pages/2_👓_Detection.py
@@ -69,15 +69,17 @@ if load_button:
    
 # taille en pixel
 pixel_size = st.sidebar.slider('Résolution (pixel)', 0, PIXEL_SIZE_MAX, st.session_state['pixel_size'], 100)
-if pixel_size:
-    st.session_state['pixel_size'] = pixel_size
-    if st.session_state['coords_bbox_Lambert'] != (None, None, None, None):
+
+# bouton de calcul
+calcul_button = st.sidebar.button('prédire')
+if calcul_button:
+   st.session_state['pixel_size'] = pixel_size
+   if st.session_state['coords_bbox_Lambert'] != (None, None, None, None):
       st.session_state['scale'] = round((st.session_state['coords_bbox_Lambert'][1] - st.session_state['coords_bbox_Lambert'][0])/st.session_state['pixel_size'], 1)
+   st.rerun()
 st.sidebar.caption('Echelle: {} m/pixel'.format(st.session_state['scale']))
 if st.session_state['scale'] != PIXEL_SCALE_REF:
    st.sidebar.warning('attendion, l\'échelle de référence est {} m/pixel'.format(PIXEL_SCALE_REF))
-
-st.sidebar.write(st.session_state['coords_bbox_Lambert'])
 
 ##############
 # prédiction #
