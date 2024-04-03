ee45c9b1cb37ca78b78906cfc24f8a6bb470f3c7
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2024-04-03 15:32:09 +0200

    Update 2_👓_Detection.py

--- pages/2_👓_Detection.py
+++ pages/2_👓_Detection.py
@@ -138,17 +138,15 @@ if calcul_button:
          #@st.cache_data(show_spinner = False)
          def get_fig_prev(xmin, ymin, pixel_size, scale, orthophoto, gdf_cadastre):
             if all((xmin, ymin, pixel_size, scale, orthophoto, gdf_cadastre is not None)):
-               _, shapes_ref, _, _, _, _, fig = affiche_contours(
+               _, _, _, _, _, _, fig = affiche_contours(
                   orthophoto, predict_YOLOv8, model_YOLO, SIZE_YOLO, 
                   (xmin, ymin, scale), gdf_shapes_ref = gdf_cadastre,
                   resolution_target = (pixel_size, pixel_size),
                   seuil = 0.05, seuil_iou = 0.01,
                   seuil_area = 10,
                   tolerance_polygone = 0.1)
-               un, deux = shapes_ref[0].xy
-               for x, y in zip(un, deux):
-                  st.write(x)
-                  st.write(y)
+               st.write(xmin)
+               st.write(ymin)
                return fig
             else:
                return None
