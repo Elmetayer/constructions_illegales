80b62feb6d4b727ab9ce90d7803eb1ccafad932c
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2024-04-16 00:54:26 +0200

    Update yolo.py

--- pages/functions/yolo.py
+++ pages/functions/yolo.py
@@ -3,6 +3,7 @@ import numpy as np
 def predict_YOLOv8(image, model, size_model, seuil):
   '''
   fonction de prédiction "flat", tous les masques sont applatis
+  utilisation du CPU et non du GPU
   on ne prédit que la classe "0", bâtiments
   '''
   res = model.predict(image, save = False, classes = [0], imgsz = size_model, conf = seuil, verbose=False)
