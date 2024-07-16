b8c307967b7b30de08987fd9ae7578f5c30df4a2
Elmetayer <bruno.lemetayer@tutanota.com>
2024-07-16 21:49:41 +0200

    Create 4_🌡️_GradCam_Unet.py

---
+++ pages/4_🌡️_GradCam_Unet.py
@@ -0,0 +1,12 @@
+import plotly.express as px
+import streamlit as st
+
+from pages.functions.gradcam import *
+from pages.functions import config
+
+# titre de la page
+st.set_page_config(page_title="GradCam Unet", page_icon="🌡️", layout = 'wide')
+
+st.write('en cours de construction ....')
+
+                                     
